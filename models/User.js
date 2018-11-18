d8e1a39db2834d3563a564b6408dd31aaf33f864
Alexander Teodor-Mazilu <Amazilu77@gmail.com>
2018-11-18 09:31:37 -0500

    first genie commit

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+    {
+        username: String,
+        password: String,
+        facebookID: String,
+        googleID: String,
+        luckyNumbers: [],
+
+        // schemas for each game user saves numbers for
+
+        numbersPlayed: {
+            // counter to tell how many sets of numbers you have saved
+            counterPick2: Number,
+            //    array of saved tickets
+            pick2: [
+                {
+                    Ticket: Number,
+                    SavedAt: Date,
+                    pick2Numbers: []
+                }
+            ]
+        },
+        experiencePoints: {
+            type: Number,
+            default: 100
+        },
+
+        wishes: {
+            type: Number,
+            default: 3
+        }
+    },
+    {
+        timestamps: {
+            createdAt: "created_at",
+            updatedAt: "updated_at"
+        }
+    }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
